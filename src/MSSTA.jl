42c6e8da314c0fa2c7c4b6fc465d91f0800c8ad2
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2023-03-18 11:11:54 +0900

    Remove TCI

--- src/MSSTA.jl
+++ src/MSSTA.jl
@@ -6,13 +6,13 @@ import ITensors.NDTensors: Tensor, BlockSparseTensor, blockview
 import SparseIR: Fermionic, Bosonic, Statistics
 import LinearAlgebra: I
 using StaticArrays
-using TensorCrossInterpolation
-import TensorCrossInterpolation: TensorCI
-import TensorCrossInterpolation as TCI
+#using TensorCrossInterpolation
+#import TensorCrossInterpolation: TensorCI
+#import TensorCrossInterpolation as TCI
 
 include("mpsedge.jl")
 include("util.jl")
-include("tci.jl")
+#include("tci.jl")
 include("quantics.jl")
 include("binaryop.jl")
 include("mul.jl")
@@ -20,6 +20,6 @@ include("mps.jl")
 include("fouriertransform.jl")
 include("imaginarytime.jl")
 include("transformer.jl")
-include("qtt.jl")
+#include("qtt.jl")
 
 end
